04910168fb0f2f9557e65baac35e0758486b3f05
Steven Novak <stevennovak@stevens-macbook-pro.local>
2016-10-25 22:09:35 -0700

    Initial Commit

---
+++ fnPlot/fnPlot.h
@@ -0,0 +1,19 @@
+//
+//  fnPlot.h
+//  fnPlot
+//
+//  Created by Steven Novak on 10/25/16.
+//  Copyright © 2016 Steven Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fnPlot.
+FOUNDATION_EXPORT double fnPlotVersionNumber;
+
+//! Project version string for fnPlot.
+FOUNDATION_EXPORT const unsigned char fnPlotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fnPlot/PublicHeader.h>
+
+
